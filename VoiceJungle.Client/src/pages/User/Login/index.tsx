74bb1c9060706c03d4e07112c31e00629e28255d
Aloento <11802769+Aloento@users.noreply.github.com>
2024-03-13 13:16:37 +0100

    Update config and default settings

--- VoiceJungle.Client/src/pages/User/Login/index.tsx
+++ VoiceJungle.Client/src/pages/User/Login/index.tsx
@@ -15,12 +15,12 @@ import {
   ProFormCheckbox,
   ProFormText,
 } from '@ant-design/pro-components';
-import { FormattedMessage, history, SelectLang, useIntl, useModel, Helmet } from '@umijs/max';
-import { Alert, message, Tabs } from 'antd';
-import Settings from '../../../../config/defaultSettings';
+import { FormattedMessage, Helmet, SelectLang, history, useIntl, useModel } from '@umijs/max';
+import { Alert, Tabs, message } from 'antd';
+import { createStyles } from 'antd-style';
 import React, { useState } from 'react';
 import { flushSync } from 'react-dom';
-import { createStyles } from 'antd-style';
+import Settings from '../../../../config/defaultSettings';
 
 const useStyles = createStyles(({ token }) => {
   return {
